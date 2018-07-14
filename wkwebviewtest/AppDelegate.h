67cecf6f75be7609ee6423aeb779fe1073e47f2f
xhkj <xhkj@xhkjdeMac-mini.local>
2018-07-14 15:03:36 +0800

    webview
    在tableview上放一个webview

---
+++ wkwebviewtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wkwebviewtest
+//
+//  Created by xhkj on 2018/6/13.
+//  Copyright © 2018年 JDKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wkwebviewtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wkwebviewtest
+//
+//  Created by xhkj on 2018/6/13.
+//  Copyright © 2018年 JDKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
